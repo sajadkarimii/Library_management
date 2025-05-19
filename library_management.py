1f5eb0a8d4d1e146fb0a2794ecc0108f8ef1557c
sajadkarimii <sajad.ssthm.k@gmail.com>
2025-05-19 17:42:12 +0330

    Librarian class

--- library_management.py
+++ library_management.py
@@ -6,6 +6,23 @@ import time
 conn = sqlite3.connect("library_mange.db")
 cursor = conn.cursor()
 
+class Librarian:
+    def __init__(self,FullName='sajad karimi',UserName='sajad',Password='2233123'):
+        self.FullName = FullName
+        self.UserName = UserName
+        self.Password = Password
+
+    def login(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        username = input("Enter username :")
+        password = input("Enter password :")
+        if (username == self.UserName and password == self.Password):
+            return True
+        else:
+            if (self.login()):
+                return True
+
+
 class Book:
     def __init__(self,Book_Name,Publisher,Author,Genre,Quantity):
         self.Book_Name = Book_Name
@@ -73,97 +90,99 @@ class Member(Person):
         
 
 def main():
-    while True:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        print("1: Books mangement\n2: Members management\n3: Borrowed management\n4: Exit")
-        x = input("Choose an option : ")
-        match x:
-            case "1":
-                while True:
+    login = Librarian()
+    if login.login():
+        while True:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("1: Books mangement\n2: Members management\n3: Borrowed management\n4: Exit")
+            x = input("Choose an option : ")
+            match x:
+                case "1":
+                    while True:
+                        os.system('cls' if os.name == 'nt' else 'clear')
+                        print("1: Add book\n2: Update book\n3: Show books\n4: Delete book\n5: Search book\n6: main page")
+                        x = input("Choose an option : ")
+                        if (x == '1'):
+                            os.system('cls' if os.name == 'nt' else 'clear')
+                            bookname = input("Enter Book Name: ")
+                            publisher = input("Enter Publisher: ")
+                            author = input("Enter Author Name: ")
+                            genre = input("Enter Genre: ")
+                            quantity = int(input("Enter Quantity: "))
+                            new_book = Book(bookname,publisher,author,genre,quantity)
+                            new_book.add_book()
+                        elif (x == '2'):
+                            os.system('cls' if os.name == 'nt' else 'clear')
+                            bookname = input("Enter Book Name you want to edit : ")
+                            bookname1 = input("Enter edited Book Name: ")
+                            publisher = input("Enter edited Publisher: ")
+                            author = input("Enter edited Author Name: ")
+                            genre = input("Enter edited Genre: ")
+                            quantity = input("Enter edited Quantity: ")
+                            new_book = Book(bookname1,publisher,author,genre,quantity)
+                            new_book.update_book(bookname)
+                        elif (x == '3'):
+                            os.system('cls' if os.name == 'nt' else 'clear')
+                            new_book = Book('','','','','')
+                            new_book.show_book()
+                            time.sleep(5)
+                        elif (x == '4'):
+                            os.system('cls' if os.name == 'nt' else 'clear')
+                            bookname = input("Enter book name to delete: ")
+                            new_book = Book(bookname,'','','','')
+                            new_book.delete_book()
+                        elif (x == '5'):
+                            os.system('cls' if os.name == 'nt' else 'clear')
+                            bookname = input("Enter book name to search : ")
+                            new_book = Book(bookname,'','','','')
+                            new_book.search_book()
+                            time.sleep(5)
+                        elif(x == '6'):
+                            break
+                case "2":
+                    while True:
+                        os.system('cls' if os.name == 'nt' else 'clear')
+                        print("1: Add member\n2: Update member\n3: Show members\n4: Delete member\n5: Search member \n6: Main page")
+                        x = input("Choose an option : ")
+                        if (x == '1'):
+                            os.system('cls' if os.name == 'nt' else 'clear')
+                            n_id = input("Enter member's National number : ")
+                            name = input("Enter member's full name : ")
+                            pnumber = input("Enter member's phone nubmer : ")
+                            jdate = input("Enter join date(yyyy-mm-dd) : ")
+                            new_member = Member(n_id,name,pnumber,jdate)
+                            new_member.add_member()
+                        elif (x == '2'):
+                            os.system('cls' if os.name == 'nt' else 'clear')
+                            n_id = input("Enter member's National number : ")
+                            name = input("Enter edited member's full name : ")
+                            pnumber = input("Enter edited member's phone nubmer : ")
+                            jdate = input("Enter edited join date(yyyy-mm-dd) : ")
+                            new_member = Member(n_id,name,pnumber,jdate)
+                            new_member.update_member()
+                        elif (x == '3'):
+                            os.system('cls' if os.name == 'nt' else 'clear')
+                            new_member = Member('','','','')
+                            new_member.show_members()
+                            time.sleep(5)
+                        elif (x == '4'):
+                            os.system('cls' if os.name == 'nt' else 'clear')
+                            n_id = input("Enter member's National number : ")
+                            new_member = Member(n_id,'','','')
+                            new_member.delete_member()
+                        elif (x == '5'):
+                            os.system('cls' if os.name == 'nt' else 'clear')
+                            fullname = input("Enter member's FullName : ")
+                            new_member = Member('',fullname,'','')
+                            new_member.search_member()
+                            time.sleep(5)
+                        elif(x == '6'):
+                            break
+                case "3":
                     os.system('cls' if os.name == 'nt' else 'clear')
-                    print("1: Add book\n2: Update book\n3: Show books\n4: Delete book\n5: Search book\n6: main page")
-                    x = input("Choose an option : ")
-                    if (x == '1'):
-                        os.system('cls' if os.name == 'nt' else 'clear')
-                        bookname = input("Enter Book Name: ")
-                        publisher = input("Enter Publisher: ")
-                        author = input("Enter Author Name: ")
-                        genre = input("Enter Genre: ")
-                        quantity = int(input("Enter Quantity: "))
-                        new_book = Book(bookname,publisher,author,genre,quantity)
-                        new_book.add_book()
-                    elif (x == '2'):
-                        os.system('cls' if os.name == 'nt' else 'clear')
-                        bookname = input("Enter Book Name you want to edit : ")
-                        bookname1 = input("Enter edited Book Name: ")
-                        publisher = input("Enter edited Publisher: ")
-                        author = input("Enter edited Author Name: ")
-                        genre = input("Enter edited Genre: ")
-                        quantity = input("Enter edited Quantity: ")
-                        new_book = Book(bookname1,publisher,author,genre,quantity)
-                        new_book.update_book(bookname)
-                    elif (x == '3'):
-                        os.system('cls' if os.name == 'nt' else 'clear')
-                        new_book = Book('','','','','')
-                        new_book.show_book()
-                        time.sleep(5)
-                    elif (x == '4'):
-                        os.system('cls' if os.name == 'nt' else 'clear')
-                        bookname = input("Enter book name to delete: ")
-                        new_book = Book(bookname,'','','','')
-                        new_book.delete_book()
-                    elif (x == '5'):
-                        os.system('cls' if os.name == 'nt' else 'clear')
-                        bookname = input("Enter book name to search : ")
-                        new_book = Book(bookname,'','','','')
-                        new_book.search_book()
-                        time.sleep(5)
-                    elif(x == '6'):
-                        break
-            case "2":
-                while True:
-                    os.system('cls' if os.name == 'nt' else 'clear')
-                    print("1: Add member\n2: Update member\n3: Show members\n4: Delete member\n5: Search member \n6: Main page")
-                    x = input("Choose an option : ")
-                    if (x == '1'):
-                        os.system('cls' if os.name == 'nt' else 'clear')
-                        n_id = input("Enter member's National number : ")
-                        name = input("Enter member's full name : ")
-                        pnumber = input("Enter member's phone nubmer : ")
-                        jdate = input("Enter join date(yyyy-mm-dd) : ")
-                        new_member = Member(n_id,name,pnumber,jdate)
-                        new_member.add_member()
-                    elif (x == '2'):
-                        os.system('cls' if os.name == 'nt' else 'clear')
-                        n_id = input("Enter member's National number : ")
-                        name = input("Enter edited member's full name : ")
-                        pnumber = input("Enter edited member's phone nubmer : ")
-                        jdate = input("Enter edited join date(yyyy-mm-dd) : ")
-                        new_member = Member(n_id,name,pnumber,jdate)
-                        new_member.update_member()
-                    elif (x == '3'):
-                        os.system('cls' if os.name == 'nt' else 'clear')
-                        new_member = Member('','','','')
-                        new_member.show_members()
-                        time.sleep(5)
-                    elif (x == '4'):
-                        os.system('cls' if os.name == 'nt' else 'clear')
-                        n_id = input("Enter member's National number : ")
-                        new_member = Member(n_id,'','','')
-                        new_member.delete_member()
-                    elif (x == '5'):
-                        os.system('cls' if os.name == 'nt' else 'clear')
-                        fullname = input("Enter member's FullName : ")
-                        new_member = Member('',fullname,'','')
-                        new_member.search_member()
-                        time.sleep(5)
-                    elif(x == '6'):
-                        break
-            case "3":
-                os.system('cls' if os.name == 'nt' else 'clear')
-                pass
-            case "4":
-                break
+                    pass
+                case "4":
+                    break
 
 if __name__ == "__main__":
     main()
